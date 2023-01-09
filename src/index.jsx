0cbab7162af76fb0f56e1ac0547ee3a5cf23ac53
Brevan Mitchell <brevanm@gmail.com>
2023-01-08 21:59:44 -0600

    Talking with database

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrtj71l3VbmET1bKGtekAsifVzRlUmgHU",
+  authDomain: "dininginformate.firebaseapp.com",
+  databaseURL: "https://dininginformate.firebaseio.com",
+  projectId: "dininginformate",
+  storageBucket: "dininginformate.appspot.com",
+  // messagingSenderId: "sender-id",
+  // appID: "app-id",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// console.log(db);
+export async function getWaitTimes() {
+  const waitTimes = collection(db, 'Waiting Times');
+  const waitSnapshot = await getDocs(waitTimes);
+  // console.log({waitSnapshot})
+  const waitList = waitSnapshot.docs.map(doc => doc.data());
+  // console.log({waitList})
+  return waitList;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
