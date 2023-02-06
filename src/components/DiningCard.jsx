958b3a0e9b5fbe95866ee8163f956e9fd9837bc4
Brevan Mitchell <brevanm@gmail.com>
2023-02-05 23:52:10 -0600

    Menu Modal now closes

--- src/components/DiningCard.jsx
+++ src/components/DiningCard.jsx
@@ -6,32 +6,29 @@ import { CircularProgress } from '@mui/material';
 export const DiningCard = (props) => {
   const { waitTime, diningHallId, stars, imageLink, diningData, loading } = props;
 
-  const mealName = ['Breakfast','Lunch','Dinner'];
+  const mealName = ['Breakfast', 'Lunch', 'Dinner'];
 
   const [isOpen, toggleOpen] = useState(false);
 
   let meal;
   const date = new Date();
   const hour = date.getHours();
-  if(hour < 11) {
+  if (hour < 11) {
     meal = 0;
-  } else if(hour < 16) {
+  } else if (hour < 16) {
     meal = 1;
   } else {
     meal = 2;
   }
-  
+
   // const featuredItems = diningData ? diningData[diningHallId][meal][0]['items'].slice(0, 3) : [];
-  const featuredItems = diningData ? diningData[diningHallId][mealName[meal].toLowerCase()][0]['items'].slice(0, 3) : [];
+  const featuredItems = diningData
+    ? diningData[diningHallId][mealName[meal].toLowerCase()][0]['items'].slice(0, 3)
+    : [];
 
   const openModal = () => {
     toggleOpen(!isOpen);
   };
-
-  const handleButtonPress = (event) => {
-    event.preventDefault();
-    toggleOpen(true)
-  }
 
   let color;
   if (waitTime < 10) {
@@ -43,64 +40,66 @@ export const DiningCard = (props) => {
   }
 
   return (
-    <section className="cards" onClick={handleButtonPress}>
-      <div>
-        <img className="hallpic" src={imageLink} alt={`Picture of ${diningHallId}`} />
-        <div className="hallname">
-          <h2>{diningHallId} Dining Hall</h2>
+    <>
+      <section className="cards" onClick={openModal}>
+        <div>
+          <img className="hallpic" src={imageLink} alt={`Picture of ${diningHallId}`} />
+          <div className="hallname">
+            <h2>{diningHallId} Dining Hall</h2>
+          </div>
         </div>
-      </div>
-      <div className="middle">
-        <div className="middleleft">
-          <div className="featureditems">
-            <div className="featuredlabel">
-              <h1>{mealName[meal]} Preview:</h1>
+        <div className="middle">
+          <div className="middleleft">
+            <div className="featureditems">
+              <div className="featuredlabel">
+                <h1>{mealName[meal]} Preview:</h1>
+              </div>
+              <div className="featureditemslist">
+                {loading ? <CircularProgress /> : null}
+                {featuredItems.map((item) => {
+                  const { name } = item;
+                  return (
+                    <div key={name} className="item">
+                      {name}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-            <div className="featureditemslist">
-              {loading ? <CircularProgress /> : null}
-              {featuredItems.map((item) => {
-                const { name } = item;
-                return (
-                  <div key={name} className="item">
-                    {name}
-                  </div>
-                );
-              })}
+            <div className="waitscore">
+              <Rating
+                name="half-rating-read"
+                value={stars}
+                precision={0.5}
+                readOnly
+                sx={{
+                  '& .MuiRating-icon': {
+                    fontSize: '5vw'
+                  }
+                }}
+              />
             </div>
           </div>
-          <div className="waitscore">
-            <Rating
-              name="half-rating-read"
-              value={stars}
-              precision={0.5}
-              readOnly
-              sx={{ 
-                '& .MuiRating-icon': {
-                fontSize: '5vw'
-                } 
-              }}
-            />
+          <div className="middleright">
+            <div className="waitlabel" style={{ backgroundColor: color }}>
+              <div className="waittime">{`${waitTime} `}</div>
+              min
+            </div>
+            <div className="more">
+              <button className="morebutton">
+                Full Menu <i className="fas fa-arrow-right"></i>
+              </button>
+            </div>
           </div>
         </div>
-        <div className="middleright">
-          <div className="waitlabel" style={{ backgroundColor: color }}>
-            <div className="waittime">{`${waitTime} `}</div>
-            min
-          </div>
-          <div className="more">
-            <button className="morebutton">
-              Full Menu <i className="fas fa-arrow-right"></i>
-            </button>
-          </div>
-          <Menu
-            open={isOpen}
-            loading={loading}
-            data={diningData}
-            diningHallId={diningHallId}
-            toggleModal={openModal}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+      <Menu
+        open={isOpen}
+        loading={loading}
+        data={diningData}
+        diningHallId={diningHallId}
+        toggleModal={openModal}
+      />
+    </>
   );
 };
