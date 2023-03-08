56e369acb028cbc498edf2daee7ee49f40dc3eed
rnewkirk <ryannewkirk2024@u.northwestern.edu>
2023-03-08 16:43:23 -0600

    remade the default unit test to match our app

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,15 +3,15 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('counter tests', () => {
-  test('Counter should be 0 at the start', () => {
+  test('the text Share your experience! should be on screen (case sensitive)', () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.getByText('Share your experience!')).toBeDefined();
   });
-
+  /*
   test('Counter should increment by one when clicked', async () => {
     render(<App />);
     const counter = screen.getByRole('button');
     fireEvent.click(counter);
     expect(await screen.getByText('count is: 1')).toBeDefined();
-  });
+  });*/
 });
